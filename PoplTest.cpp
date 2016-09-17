581a8fd47e459487121fc3cbebac3eb63e16f26e
badaix <snapcast@badaix.de>
2016-09-17 10:43:27 +0200

    switched to MIT license

--- PoplTest.cpp
+++ PoplTest.cpp
@@ -1,19 +1,9 @@
 /***
     This file is part of popl (program options parser lib)
-    Copyright (C) 2015  Johannes Pohl
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    Copyright (C) 2015-2016 Johannes Pohl
+    
+    This software may be modified and distributed under the terms
+    of the MIT license.  See the LICENSE file for details.
 ***/
 
 #include "popl.hpp"
