722a55dd93cd8843bbe1d0016019448f1197daec
badaix <johannes.pohl@badaix.de>
2018-05-13 23:19:38 +0200

    use char* argv[] instead of char** argv

--- include/popl.hpp
+++ include/popl.hpp
@@ -196,7 +196,7 @@ public:
 	template<typename T, typename... Ts>
 	std::shared_ptr<T> add(Ts&&... params);
 
-	void parse(int argc, const char * const * argv);
+	void parse(int argc, const char * const argv[]);
 	std::string help(const Visibility& max_visibility = Visibility::normal) const;
 	std::string description() const;
 	const std::vector<Option_ptr>& options() const;
@@ -659,7 +659,7 @@ inline std::shared_ptr<T> OptionParser::get_option(char short_opt) const
 }
 
 
-inline void OptionParser::parse(int argc, const char * const * argv)
+inline void OptionParser::parse(int argc, const char * const argv[])
 {
 	unknown_options_.clear();
 	non_option_args_.clear();
